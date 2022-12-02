17c3b4e48c60952c128d2484d0653b48e0be5a2a
najamoe <naja0718@stud.kea.dk>
2022-12-02 12:13:00 +0100

    edit on chairmanmenu

--- src/main/java/org/example/UserInterface.java
+++ src/main/java/org/example/UserInterface.java
@@ -218,7 +218,7 @@ public class UserInterface {
             }else if (chairmanChoice == 5) {
                 System.out.println("List of members:" + "\n");
                 for (Member member : controller.getMembers()) {
-                    System.out.println("First name: " + member.getFirstname() + "\n" + "Last name: " + member.getLastname() + "\n" + "Date, month and year of birth: "
+                    System.out.println("Full name " + member.getFirstname() + "\n" + member.getLastname() + "\n" + "Date, month and year of birth: "
                             + member.getBirthDate() + "\n" + "Address: " + member.getAddress() + " " + member.getPostalCode() + " " + member.getCity() + "\n"
                             + "Phone number: " + member.getPhoneNo() + "\n" + "Email address: " + member.geteMail() +
                             "\n" + "Membership typer: " + member.isPassive() + ", " + member.isJunior() + ", " + member.isExercise() + "\n" + "Has paid the subscription: " + member.isHasPaid() + "\n");
