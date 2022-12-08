63408478ddd0652e9f12851bcf61548979c0d504
gottbrecht <simo79j5@stud.kea.dk>
2022-12-08 10:23:40 +0100

    DatabaseTest

--- src/test/java/fff/delfinen/DatabaseTest.java
+++ src/test/java/fff/delfinen/DatabaseTest.java
@@ -20,6 +20,7 @@ class DatabaseTest {
 
     @Test
     void registration() {
+        database.getMembers().clear(); //Hver gang man kører databasetest, skriv: database.getmembers().clear(); - Slettes igen efter kørt test
         database.addMember("Simone",  "Gottbrecht", LocalDate.ofEpochDay(1992), "Hejvej",
                 2100, "København", 28891106, "simo79j5@stud.kea.dk", true, true, true, true);
         database.addMember("Simone",  "Gottbrecht", LocalDate.ofEpochDay(1992), "Hejvej",
